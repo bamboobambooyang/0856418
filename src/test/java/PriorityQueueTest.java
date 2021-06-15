3c93d36a9c13d9088960a782e585e664536716f3
bamboobambooyang <70704373+bamboobambooyang@users.noreply.github.com>
2021-06-15 20:57:29 +0800

    Create PriorityQueueTest.java

---
+++ src/test/java/PriorityQueueTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+
+
+public class PriorityQueueTest {
+    static Stream<Arguments> stringIntAndListProvider(){
+        //  return stream
+        return Stream.of(
+                arguments(new int[]{3,1,2},new int[] {1,2,3}),
+                arguments(new int[]{5,4,6},new int[] {4,5,6}),
+                arguments(new int[]{9,7,8},new int[] {7,8,9}),
+                arguments(new int[]{6,4,2},new int[] {2,4,6}),
+                arguments(new int[]{9,6,3},new int[] {3,6,9})
+        );
+    }
+
+    @ParameterizedTest(name="#{index} -Test with Argument={0},{1}")
+    @MethodSource("stringIntAndListProvider")
+    public void PriorityQueue_RunTest(int [] random_array,int[] correct_array){
+        PriorityQueue<Integer> test = new PriorityQueue<Integer>();
+        int index = 0;
+        Integer s;
+        int [] result = new int[random_array.length];
+        for (int i =0 ; i<random_array.length;i++){
+            test.add(random_array[i]);
+        }
+        //random_array add to priorityqueue   get result
+        for(int i = 0 ; i< random_array.length;i++){
+            result[i] = test.poll();
+
+        }
+        assertArrayEquals(correct_array,result);
+    }
+
+    //三個例外
+    @Test
+    public void InitialCapacityTest(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new PriorityQueue(-1, null);
+        });
+    }
+
+    @Test
+    public void OfferTest(){
+        Exception exception = assertThrows(NullPointerException.class, () ->{
+            new PriorityQueue().offer(null);
+        });
+    }
+
+    @Test
+    public void ForEachRemainingTest(){
+        Exception exception = assertThrows(NullPointerException.class, () ->{
+            new PriorityQueue().forEach(null);
+        });
+    }
+}
